0370a05ba157bddf4037e4ca828d327f056a93ee
mari152006 <116729001+mari152006@users.noreply.github.com>
2023-09-13 08:50:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function tocaSompom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSompom (idElementoAudio) {
+ document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorALL('.tecla');
+
+//para 
+for (let contador = 0; Contador <ListaDeTeclas.length;contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = teclas.classlist[1]
+    const idAudio = 'Som_&(instrumento)';
+
+  
+    teclas.onclick = function (){
+          tocaSom(idAudio)
+    }
+
+    console.log(contador);
+
+}
